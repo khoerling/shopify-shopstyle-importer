8f5e36a68ba61cdebbe0ae3fcd85072d7b311ad5
mr. keith <keith@hoerling.com>
2019-04-08 21:08:24 -0700

    remove apollo bits
    - hoping to fix production runtime

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import App, {Container} from 'next/app'
-import withApolloClient from '../lib/with-apollo-client'
-import { ApolloProvider } from 'react-apollo'
 import { Page, AppProvider } from '@shopify/polaris'
 import queryString from 'query-string'
 import '@shopify/polaris/styles.css'
@@ -13,17 +11,15 @@ class MyApp extends App {
       apiKey     = process.env.NEXT_STATIC_SHOPIFY_API_KEY,
       query      = global.isClient ? queryString.parse(window.location.search) : '',
       shopOrigin = query.shop || 'http://live-tinted.myshopify.com',
-      {Component, pageProps, apolloClient} = this.props
+      {Component, pageProps} = this.props
     return (
       <Container>
         <AppProvider shopOrigin={isClient ? window : ''}>
-          <ApolloProvider client={apolloClient}>
             <Component {...pageProps} />
-          </ApolloProvider>
         </AppProvider>
       </Container>
     )
   }
 }
 
-export default withApolloClient(MyApp)
+export default MyApp
