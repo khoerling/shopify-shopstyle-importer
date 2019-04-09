cd44d1c647a1a7bd8aacd21b691db13165e44773
mr. keith <keith@hoerling.com>
2019-04-08 21:59:44 -0700

    Revert "remove apollo bits"
    This reverts commit 8f5e36a68ba61cdebbe0ae3fcd85072d7b311ad5.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import App, {Container} from 'next/app'
+import withApolloClient from '../lib/with-apollo-client'
+import { ApolloProvider } from 'react-apollo'
 import { Page, AppProvider } from '@shopify/polaris'
 import queryString from 'query-string'
 import '@shopify/polaris/styles.css'
@@ -11,15 +13,17 @@ class MyApp extends App {
       apiKey     = process.env.NEXT_STATIC_SHOPIFY_API_KEY,
       query      = global.isClient ? queryString.parse(window.location.search) : '',
       shopOrigin = query.shop || 'http://live-tinted.myshopify.com',
-      {Component, pageProps} = this.props
+      {Component, pageProps, apolloClient} = this.props
     return (
       <Container>
         <AppProvider shopOrigin={isClient ? window : ''}>
+          <ApolloProvider client={apolloClient}>
             <Component {...pageProps} />
+          </ApolloProvider>
         </AppProvider>
       </Container>
     )
   }
 }
 
-export default MyApp
+export default withApolloClient(MyApp)
